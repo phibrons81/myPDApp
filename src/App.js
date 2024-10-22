ae49e8292fcecb9d1a18e355b7e3ae34fb600b0c
Your Name <you@example.com>
2024-10-22 20:56:41 +0200

    update_003

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import ModelViewer from "./components/ModelViewer";
+import CollapsibleTable from "./components/Table";
+import GaugeChart from "./components/Gauge";
+import ForceChart from "./components/LineChart";
+import Navbar from "./components/Navbar";
+import Recommender from "./components/Recommender";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route className="main" path="/" element={<Home />} />
+        <Route path="/chart" element={<GaugeChart />} />
+        <Route path="/lineChart" element={<ForceChart />} />
+        <Route path="/table" element={<CollapsibleTable />} />
+        <Route
+          path="/model_viewer"
+          element={<ModelViewer file={"./static/models/FLCA_3P.glb"} />}
+        />
+        <Route path="/lineChart" element={<CollapsibleTable />} />
+        <Route path="/recommender" element={<Recommender />} />
+      </Routes>
+    </Router>
   );
 }
 
