ea9960401fb3166474f2b47328f4fd3d0e3e0404
phibrons81 <phibrons@gmail.com>
2024-10-24 21:07:57 +0200

    commit 006

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import Landing from "./components/Landing";
 import ModelViewer from "./components/ModelViewer";
 import CollapsibleTable from "./components/Table";
 import GaugeChart from "./components/Gauge";
 import ForceChart from "./components/LineChart";
 import Navbar from "./components/Navbar";
 import Recommender from "./components/Recommender";
+import SignIn from "./components/signIn/SignIn";
 import "./App.css";
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route className="main" path="/" element={<Home />} />
+        <Route className="main" path="/" element={<Landing />} />
         <Route path="/chart" element={<GaugeChart />} />
         <Route path="/lineChart" element={<ForceChart />} />
         <Route path="/table" element={<CollapsibleTable />} />
@@ -25,6 +26,7 @@ function App() {
         />
         <Route path="/lineChart" element={<CollapsibleTable />} />
         <Route path="/recommender" element={<Recommender />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   );
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,16 +1,39 @@
-// src/components/Landing.js
 import React from 'react';
-import { Button } from '@mui/material';
+import solarTunnel from '../static/vids/217643_small.mp4'
+import galaxyFusion from '../static/vids/218490_small.mp4'
+import cicleGlalaxy from '../static/vids/170655-843752693_small.mp4'
+import BWExplosion from '../static/vids/98191-647151551_small.mp4'
+import CookieBanner from './Cookie';
+import { Box, Button, Typography } from '@mui/material';
 
 function Landing() {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <Button variant="contained" color="primary">
-        Learn More
-      </Button>
+    <div className='main'>
+        <div className="overlay"></div>
+        
+        <video src={solarTunnel} autoPlay loop muted />
+        <div className="content">
+            <h1 className='welcome'>Welcome</h1>
+            <p className='welcome'>To Next Generation Product Development</p>
+            <Box gap={5} className='Buttonwelcome' display="flex">
+                <Button fontSize={16} fontWeight={700} color='white' variant="outlined">
+                Sign In
+                </Button>
+                <Button fontSize={16} fontWeight={700} color='white' variant="outlined">
+                Register
+                </Button>
+                <Button fontSize={16} fontWeight={700} color='white' variant="outlined">
+                Discover
+                </Button>
+              
+            </Box>
+            <div className='linewelcome'></div>
+        </div>
+        
+        <CookieBanner/>
+
     </div>
-  );
+  )
 }
 
 export default Landing;
